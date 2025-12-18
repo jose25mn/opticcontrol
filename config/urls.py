7f4d88935db0860a689b1379b83771b138ef92db
Jose Inácio Mota Nazario <113730214+jose25mn@users.noreply.github.com>
2025-12-18 09:31:01 -0300

    Add inventory views and update urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from apps.inventory import views as inventory_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    # ROTA RAIZ (IMPORTANTE PRO RENDER)
+    path("", inventory_views.dashboard, name="home"),
+
+    # INVENTORY
     path("", include("apps.inventory.urls")),
 ]
